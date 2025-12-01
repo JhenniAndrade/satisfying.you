8d97722502e5e6c7a257d5533c920a134a532980
Fabian Uchoa <fabian@forlogic.net>
2025-11-30 23:40:56 -0300

    ultimas atualizacoes

--- src/firebase/storage.js
+++ src/firebase/storage.js
@@ -2,22 +2,18 @@ import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { storage_mod } from './config'; 
 
 export const uploadImageToFirebase = async (uri, pesquisaId) => {
+  try {
     const response = await fetch(uri);
     const blob = await response.blob();
-    
-    const date = new Date().getTime();
-    const fileName = `${pesquisaId}_${date}.jpg`;
-    
+
+    const fileName = `${pesquisaId}_${Date.now()}.jpg`;
     const storageRef = ref(storage_mod, `images/${pesquisaId}/${fileName}`);
 
-    try {
-        const snapshot = await uploadBytes(storageRef, blob);
-        const downloadURL = await getDownloadURL(snapshot.ref);
-        
-        return downloadURL;
+    await uploadBytes(storageRef, blob);
+    return await getDownloadURL(storageRef);
 
-    } catch (error) {
-        console.error("Erro ao fazer upload para o Firebase Storage:", error);
-        return null;
-    }
+  } catch (error) {
+    console.error("Erro ao fazer upload:", error);
+    return null;
+  }
 };
